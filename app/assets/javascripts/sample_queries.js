12b0013a841ab56ae8ded79b4acfafe7ceec9922
Michael Hayden <michael-hayden@sbcglobal.net>
2018-02-26 11:19:12 -0800

    fix for back button to DataTables to avoid re-init

--- app/assets/javascripts/sample_queries.js
+++ app/assets/javascripts/sample_queries.js
@@ -9,26 +9,22 @@ function sample_queries_init() {
 
 // samples controller#index init function
 function sample_queries_index_init() {
+
   var dt = $("table.data-table").DataTable( {
+    // define column 0 as hidden but searchable
     columnDefs: [ { targets: [0], visible: false, searchable: true } ]
   });
 
-  // attach the select to the DOM and filter on change
-  $(".source-select").append(select_source_html()).find('select').on('change', function() {
-logger("Got select change");
+  // fix for back button, destroy DataTables before caching the page
+  document.addEventListener("turbolinks:before-cache", function() {
+    if (dt !== null) {
+     dt.destroy();
+     dt = null;
+    }
+  });
+
+  // search filter and redraw on change
+  $(".source-select select").on('change', function() {
     dt.columns(0).search(this.value).draw();
   });
-}
-
-function select_source_html() {
-  var html =
-  '<label class="col-auto col-form-label">Sample Type:</label>' +
-  '<div class="col-auto">' +
-    '<select class="form-control">' +
-      '<option value="">All</option>' +
-      '<option value="is-source">Source</option>' +
-      '<option value="is-dissection">Dissection</option>' +
-    '</select>' +
-  '</div>';
-  return $(html);
 }
