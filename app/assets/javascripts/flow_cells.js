8c6a3e31013e2937101fafd999e38e17164662e9
Sue M Grimes <sgrimes@stanford.edu>
2019-08-29 12:21:03 -0700

    Working version of flow_cell machine_type/seq_kits dropdowns

--- app/assets/javascripts/flow_cells.js
+++ app/assets/javascripts/flow_cells.js
@@ -1,17 +1,17 @@
 // Routines to help manage flow cell form responses
-
-jQuery(function() {
+function flow_cells_init() {
     var seq_kits;
     seq_kits = $('#flow_cell_sequencing_kit').html();
     console.log(seq_kits);
-    return $('#flow_cell_machine_type').change(function() {
+
+    // For edit form, or after validation error on create, have existing machine type
+    handle_existing_machine_type(seq_kits)
+
+    $('#flow_cell_machine_type').change(function() {
         var machine_type, options;
-        machine_type = $('#flow_cell_machine_type :selected').text();
-        if (machine_type == "") {
-            options = $(seq_kits).html()
-        } else {
-            options = $(seq_kits).filter("optgroup[label=" + machine_type + "]").html();
-        }
+        machine_type = $('#flow_cell_machine_type :selected').val();
+
+        options = kit_options_for_machine_type(seq_kits, machine_type)
         console.log(options);
         if (options) {
             return $('#flow_cell_sequencing_kit').html(options);
@@ -19,10 +19,33 @@ jQuery(function() {
             return $('#flow_cell_sequencing_kit').empty();
         }
     });
-});
+};
 
+// After error the machine type may already be selected
+// If so initialize the sequencing kit dropdown accordingly
+function handle_existing_machine_type(seq_kits) {
+    var selected_kit = $("select#flow_cell_sequencing_kit");
+    if (selected_kit.val() != "") { return }
 
-function flow_cells_init() {
-//    sequencer_kit_init()
+    var selected_machine = $("select#flow_cell_machine_type").val();
+    if (selected_machine === undefined) { selected_machine = $("input#flow_cell_machine_type").val() }
+    logger("Have machine type "+selected_machine)
+
+    var options =  kit_options_for_machine_type(seq_kits, selected_machine);
+    console.log(options);
+    if (options) {
+        return $('#flow_cell_sequencing_kit').html(options);
+    } else {
+        return $('#flow_cell_sequencing_kit').empty();
+    }
 }
+
+function kit_options_for_machine_type(seq_kits, machine_type) {
+    if (machine_type == "") {
+        return seq_kits
+    } else {
+        return $(seq_kits).filter("optgroup[label=" + machine_type + "]").html();
+    }
+}
+
 
