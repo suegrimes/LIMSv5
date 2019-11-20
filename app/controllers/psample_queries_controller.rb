ebf6a11a7f039e13b6a6e0fa742a0693e0b5180f
suegrimes <sgrimes@stanford.edu>
2019-11-20 13:29:14 -0800

    Bug fix for processed sample query/MRN search

--- app/controllers/psample_queries_controller.rb
+++ app/controllers/psample_queries_controller.rb
@@ -68,7 +68,7 @@ protected
 
     if !params[:psample_query][:mrn].blank?
       patient_id = Patient.find_id_using_mrn(params[:psample_query][:mrn])
-      @where_select.push("samples.patient.id = #{patient_id ||= 0}")
+      @where_select.push("samples.patient_id = #{patient_id ||= 0}")
     end
 
     @where_select, @where_values = build_sql_where(params[:psample_query], PsampleQuery::QUERY_FLDS, @where_select, @where_values)
