37a2901b4c0e4b001437e0c605930407f26735ad
suegrimes <sgrimes@stanford.edu>
2020-05-06 23:50:19 -0700

    Bug fix for processed sample containers - only execute js storage init functions for new/edit actions

--- app/assets/javascripts/processed_samples.js
+++ app/assets/javascripts/processed_samples.js
@@ -3,7 +3,11 @@
 //   <controller>_<action>_init() => controller/action specific init
 
 // processed_samples new init
-function processed_samples_init() {
+function processed_samples_new_init() {
+    storage_container_init();
+}
+
+function processed_samples_edit_init() {
     storage_container_init();
     edit_storage_container_init();
 }
