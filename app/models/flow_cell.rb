b0332cc95f9c4103bf7c4fd22793be9133a4fde1
Sue M Grimes <sgrimes@stanford.edu>
2021-03-18 14:52:01 -0700

    Add new machine type: PromethION, with max 24 lanes

--- app/models/flow_cell.rb
+++ app/models/flow_cell.rb
@@ -45,8 +45,9 @@ class FlowCell < ApplicationRecord
   scope :desc_by_run, -> { order("flow_cells.seq_run_nr DESC")}
   scope :desc_by_date, -> { order("flow_cells.flowcell_date DESC")}
   
-  DEFAULT_MACHINE_TYPE = 'MiSeq'
-  NR_LANES = {:iSeq => 1, :Genius => 1, :MinION => 1, :MiSeq => 1, :NextSeq => 1, :NovaSeq => 4, :GAIIx => 8, :HiSeq => 8}
+  DEFAULT_MACHINE_TYPE = 'iSeq'
+  NR_LANES = {:iSeq => 1, :Genius => 1, :MinION => 1, :MiSeq => 1, :NextSeq => 1, :NovaSeq => 4, :GAIIx => 8, :HiSeq => 8,
+              :PromethION => 24}
   STATUS = %w{F R S Q N X}
   RUN_NR_TYPES = %w{LIMS Illumina}
   
